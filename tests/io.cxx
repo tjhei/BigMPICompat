b4186feb2ed5c0ca9c96468bf217507100781204
Timo Heister <timo.heister@gmail.com>
2022-04-16 22:49:47 -0400

    simplify read/write test

--- tests/io.cxx
+++ tests/io.cxx
@@ -5,7 +5,7 @@
 #include <vector>
 
 void
-test_write(const std::uint64_t n_bytes, const std::string &command)
+test_read_write(const std::uint64_t n_bytes, const std::string &command)
 {
   int ierr;
   int myid, ranks;
@@ -20,17 +20,21 @@ test_write(const std::uint64_t n_bytes, const std::string &command)
   ierr = MPI_Info_create(&info);
   CheckMPIFatal(ierr);
 
-  ierr = MPI_File_open(
-    MPI_COMM_WORLD, "io.data", MPI_MODE_CREATE | MPI_MODE_WRONLY, info, &fh);
+  ierr =
+    MPI_File_open(MPI_COMM_WORLD,
+                  "io.data",
+                  MPI_MODE_CREATE | MPI_MODE_RDWR | MPI_MODE_DELETE_ON_CLOSE,
+                  info,
+                  &fh);
   CheckMPIFatal(ierr);
 
   std::vector<char> buffer(n_bytes, '?');
   buffer[0] = 'A' + myid;
 
-  if (command == "write_at")
+  if (command == "at")
     ierr = BigMPICompat::MPI_File_write_at_c(
       fh, offset, buffer.data(), buffer.size(), MPI_CHAR, MPI_STATUS_IGNORE);
-  else if (command == "write_at_all")
+  else if (command == "at_all")
     ierr = BigMPICompat::MPI_File_write_at_all_c(
       fh, offset, buffer.data(), buffer.size(), MPI_CHAR, MPI_STATUS_IGNORE);
   else if (command == "write_ordered")
@@ -48,6 +52,28 @@ test_write(const std::uint64_t n_bytes, const std::string &command)
   CheckMPIFatal(ierr);
 
   MPI_Barrier(MPI_COMM_WORLD);
+
+  {
+    std::vector<char> buffer(n_bytes);
+    if (command == "at")
+      ierr = BigMPICompat::MPI_File_read_at_c(
+        fh, offset, buffer.data(), buffer.size(), MPI_CHAR, MPI_STATUS_IGNORE);
+    else if (command == "at_all")
+      ierr = BigMPICompat::MPI_File_read_at_all_c(
+        fh, offset, buffer.data(), buffer.size(), MPI_CHAR, MPI_STATUS_IGNORE);
+    else
+      MPI_Abort(MPI_COMM_WORLD, 1);
+
+    CheckMPIFatal(ierr);
+
+    if (buffer[0] != 'A' + myid || buffer[1] != '?' ||
+        buffer[n_bytes - 1] != '?')
+      {
+        std::cerr << "io: " << command << " read" << command << " incorrect."
+                  << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, 1);
+      }
+  }
 
   if (myid == 0)
     {
@@ -68,67 +94,13 @@ test_write(const std::uint64_t n_bytes, const std::string &command)
 }
 
 
-void
-test_read(const std::uint64_t n_bytes, const std::string &command)
-{
-  int ierr;
-  int myid, ranks;
-  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-  MPI_Comm_size(MPI_COMM_WORLD, &ranks);
-  assert(ranks == 2);
-
-  std::uint64_t offset = myid * n_bytes;
-
-  MPI_File fh;
-  MPI_Info info;
-  ierr = MPI_Info_create(&info);
-  CheckMPIFatal(ierr);
-
-  ierr = MPI_File_open(MPI_COMM_WORLD,
-                       "io.data",
-                       MPI_MODE_DELETE_ON_CLOSE | MPI_MODE_RDONLY,
-                       info,
-                       &fh);
-  CheckMPIFatal(ierr);
-
-  std::vector<char> buffer(n_bytes, '?');
-
-  if (command == "read_at")
-    ierr = BigMPICompat::MPI_File_read_at_c(
-      fh, offset, buffer.data(), buffer.size(), MPI_CHAR, MPI_STATUS_IGNORE);
-  else if (command == "read_at_all")
-    ierr = BigMPICompat::MPI_File_read_at_all_c(
-      fh, offset, buffer.data(), buffer.size(), MPI_CHAR, MPI_STATUS_IGNORE);
-  else
-    MPI_Abort(MPI_COMM_WORLD, 1);
-
-  CheckMPIFatal(ierr);
-
-  // ierr = MPI_File_sync(fh);
-  // CheckMPIFatal(ierr);
-
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  ierr = MPI_File_close(&fh);
-  CheckMPIFatal(ierr);
-}
-
 int
 main(int argc, char *argv[])
 {
   MPI_Init(&argc, &argv);
 
-  test_write((1ULL << 32) + 2, "write_at");
-  std::cout << "in script write at done. " << std::endl;
-
-  test_read((1ULL << 32) + 2, "read_at");
-  std::cout << "in script read at done. " << std::endl;
-
-  test_write((1ULL << 32) + 2, "write_at_all");
-  std::cout << "in script write at all done. " << std::endl;
-
-  test_read((1ULL << 32) + 2, "read_at_all");
-  std::cout << "in script read at all done. " << std::endl;
+  test_read_write((1ULL << 32) + 2, "at");
+  test_read_write((1ULL << 32) + 2, "at_all");
 
   MPI_Finalize();
   return 0;
